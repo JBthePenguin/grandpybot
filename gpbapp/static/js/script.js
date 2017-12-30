44983f30a15cc5c10ab05ff0c181aeb1883d04a9
JBthePenguin <jblab976@gmail.com>
2017-12-31 01:08:52 +0300

    Add minitel on right side that appear  on click switch

--- gpbapp/static/js/script.js
+++ gpbapp/static/js/script.js
@@ -1,7 +1,24 @@
 // CAROUSEL
 
 $(function (){
-      $('#myCarousel').carousel();
+    $('#myCarousel').carousel();
 });
 
 $('#myCarousel').carousel({interval: 10000});
+
+
+// MINITEL
+
+function click_on_switch() {
+    if ($("#minitel-screen").css("display") == "none"){
+        $("#myCarousel").css("display", "none");
+        $("#minitel-screen").css("display", "inline-block");
+        $("#touch_minitel").css("display", "inline-block");
+        $('#InputToFocus').focus();
+    } else {
+        $("#minitel-screen").css("display", "none");
+        $("#touch_minitel").css("display", "none");
+        $("#myCarousel").css("display", "inline-block");
+    }
+    
+}
