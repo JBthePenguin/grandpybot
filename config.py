67484387ba4ce774e765e1e0af435bbf7c132acd
JBthePenguin <jblab976@gmail.com>
2017-12-30 12:43:45 +0300

    hello world granpybot flask

---
+++ config.py
@@ -0,0 +1,9 @@
+# To generate a new secret key:
+# >>> import random, string
+# >>> "".join([random.choice(string.printable) for _ in range(24)])
+
+SECRET_KEY = ""
+
+GM_APP_ID = 1234
+
+WIKI_APP_ID = 5678
