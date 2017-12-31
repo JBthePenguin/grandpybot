e1fbfe976037a2b0ac017f8981bb5e49bb9f2843
JBthePenguin <jblab976@gmail.com>
2017-12-31 05:27:51 +0300

    Add script to put the user message in the box after input

--- gpbapp/static/js/script.js
+++ gpbapp/static/js/script.js
@@ -1,5 +1,6 @@
 // CAROUSEL
 
+
 $(function (){
     $('#myCarousel').carousel();
 });
@@ -22,3 +23,30 @@ function click_on_switch() {
     }
     
 }
+
+
+// create and add user's message
+function create_add_msg_card($new_msg){
+    var $new_card = $("<div>").html("<i class='fa fa-user-circle-o fa-2x'></i>");
+    $new_card.addClass("msg_user col-md-11 card alert-success text-center");
+    $new_card.append($new_msg);
+    $("#dialog-area").append($new_card);
+    document.getElementById('dialog-area').scrollTop = document.getElementById('dialog-area').scrollHeight;
+}
+
+// event click on button
+
+function click_btn_dialog(){
+    var $input_user = $("#input_dialog").val();
+    var $new_msg = $("<p>").html($input_user);
+    create_add_msg_card($new_msg);
+    $("#input_dialog").val("");
+}
+
+// event return key
+$("#input_dialog").keyup(function(e) {
+    if(e.keyCode == 13) { // KeyCode return key
+        click_btn_dialog();
+    }
+});
+
